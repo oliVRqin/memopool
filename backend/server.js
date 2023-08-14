28ab6e329620c3076535c00d875a34e4b0ecf6c3
Oliver <oliver.qin1@gmail.com>
2023-08-14 00:46:29 -0700

    first message went through POST request (hello memepool) + rough frontend built

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const fs = require('fs');
 require('dotenv').config()
 
 const app = express();
@@ -8,8 +9,52 @@ const port = process.env.PORT;
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+// Get random memos from database
+app.get('/random', (req, res) => {
+    fs.readFile('data.json', (err, data) => {
+      if (err) {
+        console.log(err);
+        res.status(500).send('Error reading data');
+        return;
+      }
+
+      const memos = JSON.parse(data);
+      if (memos.length === 0) {
+        res.status(404).send('No memos found');
+        return;
+      }
+
+      const randomMemo = memos[Math.floor(Math.random() * memos.length)];
+      console.log("randomMemo: ", randomMemo);
+      res.json(randomMemo);
+    });
+});
+
+// POST new memo to database
+app.post('/', (req, res) => {
+    const newData = req.body;
+    // Read existing data from JSON
+    fs.readFile('./data.json', 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+            res.status(500).send('Server error');
+            return;
+        }
+        let jsonData = JSON.parse(data || '[]');  // If the file is empty, parse an empty array
+
+        // Push new data to the jsonData
+        jsonData.push(newData);
+
+        // Write data back to the JSON file
+        fs.writeFile('./data.json', JSON.stringify(jsonData, null, 2), (err) => {
+            if (err) {
+                console.error(err);
+                res.status(500).send('Server error');
+            } else {
+                res.status(200).send('Data written to file');
+            }
+        });
+    });
 });
 
 app.listen(port, () => {
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,8 +1,62 @@
+"use client"
+import { useState, useEffect } from 'react'
 
 export default function Home() {
+  const [formSubmitted, setFormSubmitted] = useState(false)
+  const [memoInput, setMemoInput] = useState('')
+
+  // Subject to change
+  useEffect(() => {
+    // Every 5 seconds, fetch a random memo from the API
+    setTimeout(() => {
+      const randomMemo = fetch('/', {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }).then(res => res.json())
+      console.log(randomMemo)
+    }, 5000)
+  }, [formSubmitted])
+
+  // On submit, post the memo to the API
+  const handleMemoSubmit = (e: any) => {
+    console.log('memo submitted')
+    e.preventDefault()
+    const body = {
+      id: 1, // Subject to change, maybe use uuid or make it autoincrement
+      time: new Date().toISOString(),
+      memo: memoInput
+    }
+    console.log("submitted body: ", body)
+    fetch(`http://localhost:${process.env.NEXT_PUBLIC_PORT}/`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(body),
+    }).then(response => response.json())   // or .json() if the server is sending JSON
+    .then(text => console.log(text))
+    .catch(err => console.error('Error:', err));
+    setFormSubmitted(true)
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center bg-black text-[#f5f5dc] p-24">
-      <h1 className="text-4xl font-bold text-center">MemoPool</h1>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      {
+        formSubmitted
+        ?
+          <p>Form submitted</p>
+        :
+          <>
+            <h1 className="text-4xl font-bold text-center">Welcome to your memopool</h1>
+            <p className="text-2xl text-center">This is a place where you can share your thoughts with the world</p>
+            <form onSubmit={handleMemoSubmit} className="flex flex-col items-center justify-center">
+              <input className="border-2 border-black rounded-md p-2" type="text" placeholder="Enter your memo here" value={memoInput} onInput={(e: any) => setMemoInput(e.target.value)} />
+              <button className="border-2 border-black rounded-md p-2 mt-2" type="submit">Submit</button>
+            </form>
+          </>
+      }
     </main>
   )
 }
