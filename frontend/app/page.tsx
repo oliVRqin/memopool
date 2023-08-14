19081d8a40f2a76a04dd734c3b76da26aeb8adf5
Oliver <oliver.qin1@gmail.com>
2023-08-14 10:45:03 -0700

    adding date and large QOL update on timing an d display of messages

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,16 +1,26 @@
 "use client"
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import { v4 as uuidv4 } from 'uuid';
 
 export default function Home() {
   const [formSubmitted, setFormSubmitted] = useState(false)
   const [memoInput, setMemoInput] = useState('')
+  const [fetchedMemoDate, setFetchedMemoDate] = useState('')
   const [fetchedMemoContent, setFetchedMemoContent] = useState('')
+  const lastMessageIdRef = useRef('');
 
   // Subject to change
   useEffect(() => {
     if (!formSubmitted) return;
-    setInterval(() => {
+
+    let words = memoInput.split(" ");
+    let numberOfWords = words.length;
+    let intervalTime = 5000 + (Math.floor(numberOfWords / 5) * 1000);
+
+    // Display the message you just posted first
+    setFetchedMemoContent(memoInput);
+    setFetchedMemoDate(new Date().toISOString());
+    const fetchMemo = () => {
       fetch(`http://localhost:${process.env.NEXT_PUBLIC_PORT}/random`, {
         method: 'GET',
         headers: {
@@ -21,14 +31,28 @@ export default function Home() {
         if (!res.ok) {
           throw new Error(`HTTP error! status: ${res.status}`);
         }
-        return res.json() // This returns a promise
+        return res.json();
       })
       .then(data => {
-        setFetchedMemoContent(data.memo)
+        let words = data.memo.split(" ");
+        let numberOfWords = words.length;
+      
+        // Check if the current message's ID matches the last message's ID
+        if (data.id === lastMessageIdRef.current) {
+          intervalTime = 1000; // If it does, fetch again in 1 second
+        } else {
+          intervalTime = 4000 + (Math.floor(numberOfWords / 5) * 1000);
+        }
+      
+        setFetchedMemoContent(data.memo);
+        setFetchedMemoDate(data.time);
+        lastMessageIdRef.current = data.id;
+        setTimeout(fetchMemo, intervalTime);
       })
       .catch(err => console.error('Error:', err));
-    }, 5000)
-  }, [formSubmitted])
+    }
+    setTimeout(fetchMemo, intervalTime);
+  }, [formSubmitted, memoInput])
 
   // On submit, post the memo to the API
   const handleMemoSubmit = (e: any) => {
@@ -52,6 +76,27 @@ export default function Home() {
     setFormSubmitted(true)
   }
 
+  // Converts the date string to a more readable format
+  function formatDateWithTime(dateString: string) {
+    var date = new Date(dateString);
+
+    var year = date.getFullYear();
+    var month = date.toLocaleString('default', { month: 'long' });
+    var day = date.getDate();
+
+    var hour = date.getHours();
+    var period = hour >= 12 ? 'PM' : 'AM';
+    hour = hour % 12;
+    hour = hour ? hour : 12; 
+
+    var minutes = date.getMinutes().toString();
+    minutes = parseInt(minutes) < 10 ? '0' + minutes.toString() : minutes.toString();
+
+    var formattedTime = month + ' ' + day + ', ' + year + ' — ' + hour + ':' + minutes + ' ' + period;
+
+    return formattedTime;
+  }
+
   return (
     <main className="flex min-h-screen flex-col items-center bg-black text-[#f5f5dc] p-24">
       {
@@ -59,6 +104,7 @@ export default function Home() {
         ?
           <div className='flex flex-col justify-center items-center space-y-10 w-full'>
             <p className='text-2xl'>{fetchedMemoContent}</p>
+            <p className='text-lg text-green-400'>{fetchedMemoDate ? formatDateWithTime(fetchedMemoDate) : ''}</p>
           </div>
         :
           <div className='flex flex-col justify-center items-center space-y-10 w-full'>
