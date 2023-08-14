c5f92fbedadcbbf24ace64d4a158db6d80af98b9
Oliver <oliver.qin1@gmail.com>
2023-08-14 00:21:54 -0700

    Added frontend folder contents

---
+++ frontend/README.md
@@ -0,0 +1,1 @@
+MemoPool, an app which elicits context and past thoughts serendipitiously - letting users know more about themselves and possibly discover facets of themselves they've overlooked
---
+++ frontend/app/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center bg-black text-[#f5f5dc] p-24">
+      <h1 className="text-4xl font-bold text-center">MemoPool</h1>
+    </main>
+  )
+}
